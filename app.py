56b358381ef80ae9336760531c777ff544103f45
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-22 08:52:46 -0600

    Created a folder client to make all request relationed to a client

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from http import client
 import secrets
 from flask import Flask, request, make_response
 from dbhelpers import run_statement
 from apihelpers import check_endpoint_info, check_data_sent
 from dbcreds import production_mode
 import json
-import endpoints
+import endpoints.client
 
 # calling the Flask function which will return a value that I will be used for my API
 app = Flask(__name__)
@@ -18,60 +17,17 @@ def post_client():
 
 @app.get('/api/client')
 def get_client():
-    is_valid = check_endpoint_info(request.args, ['client_id'])
-
-    if (is_valid != None):
-        return make_response(json.dumps(is_valid, default=str), 400)
-
-    results = run_statement('CALL get_client(?)', [
-                            request.args.get('client_id')])
-
-    if (type(results) == list):
-        return make_response(json.dumps(results[0], default=str), 200)
-    else:
-        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+    return endpoints.client.get()
 
 
 @app.patch('/api/client')
 def patch_client():
-
-    client_info = run_statement('CALL get_client_by_token(?)', [request.headers.get('token')])
-
-
-    update_info_client = check_data_sent(request.json, [
-                                   'email', 'first_name', 'last_name', 'image_url', 'username', 'password'], client_info[0])
-
-    results = run_statement('CALL edit_client(?,?,?,?,?,?,?)',
-    [update_info_client['email'], update_info_client['first_name'],
-    update_info_client['last_name'], update_info_client['image_url'], update_info_client['username'], 
-    update_info_client['password'], request.headers.get('token')])
-
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(results[0][0], default=str), 200)
-    elif(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad request."), 400)
-    else:
-        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+    return endpoints.client.patch()
 
 
 @app.delete('/api/client')
 def delete_client():
-    is_valid = check_endpoint_info(request.json, ['password'])
-    if(is_valid != None):
-        return make_response(json.dumps(is_valid, default=str), 400)
-
-    is_valid_header = check_endpoint_info(request.headers, ['token'])
-    if(is_valid_header != None):
-        return make_response(json.dumps(is_valid_header, default=str), 400)
-
-    results = run_statement('CALL delete_client(?,?)', [request.json.get('password'), request.headers.get('token')])
-
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(results[0][0], default=str), 200)
-    elif(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad request."), 400)
-    else:
-        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+    return endpoints.client.delete()
 
 
 @app.post('/api/client-login')
--- endpoints/client.py
+++ endpoints/client.py
@@ -4,6 +4,7 @@ import secrets
 import json
 from dbhelpers import run_statement
 
+
 def post():
     is_valid = check_endpoint_info(request.json, [
                                    'email', 'first_name', 'last_name', 'image_url', 'username', 'password'])
@@ -19,3 +20,58 @@ def post():
         return make_response(json.dumps(results, default=str), 200)
     else:
         return make_response(json.dumps("Sorry, an error has occurred."), 500)
+
+
+def get():
+    is_valid = check_endpoint_info(request.args, ['client_id'])
+
+    if (is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    results = run_statement('CALL get_client(?)', [
+                            request.args.get('client_id')])
+
+    if (type(results) == list and len(results) > 0):
+        return make_response(json.dumps(results[0], default=str), 200)
+    elif (type(results) == list and len(results) == 0):
+        return make_response(json.dumps("Sorry, this client doesn't exist.", default=str), 400)
+    else:
+        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+
+
+def patch():
+    client_info = run_statement('CALL get_client_by_token(?)', [
+                                request.headers.get('token')])
+
+    update_info_client = check_data_sent(request.json, [
+        'email', 'first_name', 'last_name', 'image_url', 'username', 'password'], client_info[0])
+
+    results = run_statement('CALL edit_client(?,?,?,?,?,?,?)',
+                            [update_info_client['email'], update_info_client['first_name'],
+                             update_info_client['last_name'], update_info_client['image_url'], update_info_client['username'],
+                                update_info_client['password'], request.headers.get('token')])
+
+    if (type(results) == list and results[0][0] == 1):
+        return make_response(json.dumps(results[0][0], default=str), 200)
+    elif (type(results) == list and results[0][0] == 0):
+        return make_response(json.dumps("Bad request."), 400)
+    else:
+        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+
+def delete():
+    is_valid = check_endpoint_info(request.json, ['password'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    is_valid_header = check_endpoint_info(request.headers, ['token'])
+    if(is_valid_header != None):
+        return make_response(json.dumps(is_valid_header, default=str), 400)
+
+    results = run_statement('CALL delete_client(?,?)', [request.json.get('password'), request.headers.get('token')])
+
+    if(type(results) == list and results[0][0] == 1):
+        return make_response(json.dumps(results[0][0], default=str), 200)
+    elif(type(results) == list and results[0][0] == 0):
+        return make_response(json.dumps("Bad request."), 400)
+    else:
+        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
