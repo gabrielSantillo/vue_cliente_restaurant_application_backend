c183ddcfb018721a452236d36f9fb4f554b957c8
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-29 11:20:04 -0600

    sending back dictionaries when the restaurant logs in

--- endpoints/restaurant_login.py
+++ endpoints/restaurant_login.py
@@ -12,10 +12,10 @@ def post():
     token = secrets.token_hex(nbytes=None)
     results = run_statement('CALL log_in_restaurant(?,?,?)', [request.json.get('email'), request.json.get('password'), token])
 
-    if(type(results) == list and len(results) > 0):
+    if(type(results) == list and len(results) != 0):
         return make_response(json.dumps(results[0], default=str), 200)
     elif(type(results) == list and len(results) == 0):
-        return make_response(json.dumps("Bad request"), 400)
+        return make_response(json.dumps("Bad login attempt. Your password or/and email are wrong."), 400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred"), 500)
 
@@ -26,9 +26,9 @@ def delete():
     
     results = run_statement('CALL delete_restaurant_token(?)', [request.headers.get('token')])
 
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(results[0][0], default=str), 200)
-    elif(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad request."), 400)
+    if(type(results) == list and results[0]['row_updated'] == 1):
+        return make_response(json.dumps(results[0], default=str), 200)
+    elif(type(results) == list and results[0]['row_updated'] == 0):
+        return make_response(json.dumps(results[0], default=str), 400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred."), 500)
