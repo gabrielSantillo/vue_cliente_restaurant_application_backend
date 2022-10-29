7042d83c08aa155ebb7694ae02e3cb258d2b910b
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-29 14:06:45 -0600

    Changing the patch request when editting a menu item

--- endpoints/menu_item.py
+++ endpoints/menu_item.py
@@ -15,10 +15,10 @@ def post():
 
     results = run_statement('CALL add_menu_item(?,?,?,?,?)', [request.json.get('description'), request.json.get('image_url'), request.json.get('name'), request.json.get('price'), request.headers.get('token')])
 
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(results[0][0], default=str), 200)
-    if(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad request"), 400)
+    if(type(results) == list and results[0]['row_updated'] == 1):
+        return make_response(json.dumps(results[0], default=str), 200)
+    if(type(results) == list and results[0]['row_updated'] == 0):
+        return make_response(json.dumps(results[0], default=str), 400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred."), 500)
 
@@ -29,7 +29,7 @@ def get():
 
     results = run_statement('CALL get_menu_item(?)', [request.args.get('restaurant_id')])
 
-    if(type(results) == list and len(results) > 0):
+    if(type(results) == list and len(results) != 0):
         return make_response(json.dumps(results[0], default=str), 200)
     elif(type(results) == list and len(results) == 0):
         return make_response(json.dumps("Wrong restaurant id."), 400)
@@ -45,14 +45,22 @@ def patch():
     if(is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    update_menu_item = check_data_sent(request.json, ['menu_id', 'description', 'image_url', 'name', 'price'], menu_info[0])
+    for item in menu_info:
+        if(item['id'] == int(request.json.get('menu_id'))):
+            is_valid = item
 
-    results = run_statement('CALL edit_menu_item(?,?,?,?,?,?)', [update_menu_item['description'], update_menu_item['image_url'], update_menu_item['name'], update_menu_item['price'], request.headers.get('token'),update_menu_item['menu_id']])
+    update_menu_item = check_data_sent(request.json, menu_info[0], ['menu_id', 'description', 'image_url', 'name', 'price'])
 
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(results[0][0], default=str), 200)
-    elif(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad request"), 400)
+    if(request.json.get('name') == None):
+        results = run_statement('CALL edit_menu_item_but_name(?,?,?,?,?)', [update_menu_item['description'], update_menu_item['image_url'], update_menu_item['price'], request.headers.get('token'),update_menu_item['menu_id']])
+    else:
+        results = run_statement('CALL edit_menu_item(?,?,?,?,?,?)', [update_menu_item['description'], update_menu_item['image_url'], update_menu_item['name'], update_menu_item['price'], request.headers.get('token'),update_menu_item['menu_id']])
+    
+
+    if(type(results) == list and results[0]['row_updated'] == 1):
+        return make_response(json.dumps(results[0], default=str), 200)
+    elif(type(results) == list and results[0]['row_updated'] == 0):
+        return make_response(json.dumps(results, default=str), 400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred"), 500)
 
@@ -68,9 +76,9 @@ def delete():
 
     results = run_statement('CALL delete_menu_item(?,?)', [request.json.get('menu_id'), request.headers.get('token')])
 
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(results[0][0], default=str), 200)
-    elif(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad request"), 400)
+    if(type(results) == list and results[0]['row_updated'] == 1):
+        return make_response(json.dumps(results[0], default=str), 200)
+    elif(type(results) == list and results[0]['row_updated'] == 0):
+        return make_response(json.dumps(results[0], default=str), 400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred."), 500)
